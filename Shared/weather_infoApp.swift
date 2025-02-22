87a7305a9872e5abf41c4211b6136e2470b00c6d
vignesh <johnvick281997@gmail.com>
2025-02-22 11:29:44 +0530

    Initial Commit

---
+++ Shared/weather_infoApp.swift
@@ -0,0 +1,20 @@
+//
+//  weather_infoApp.swift
+//  Shared
+//
+//  Created by vignesh on 22/02/25.
+//
+
+import SwiftUI
+
+@main
+struct weather_infoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
