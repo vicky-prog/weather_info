161be75dfb5d329928eee57a4526d8e4891de1a4
vignesh <johnvick281997@gmail.com>
2025-02-23 16:22:52 +0530

    text alignment changed

--- Shared/views/WeatherView.swift
+++ Shared/views/WeatherView.swift
@@ -13,26 +13,42 @@ struct WeatherView: View {
     @StateObject private var viewModel = WeatherViewModel()
 
     var body: some View {
-        VStack {
+        VStack(alignment:.leading,spacing: 2) {
             if let weather = viewModel.weather {
                 Text("🌍 \(weather.location.name), \(weather.location.country)")
-                    .font(.title)
-                    .padding()
-
-                Text("🌡 \(weather.current.temp_c, specifier: "%.1f")°C | \(weather.current.temp_f, specifier: "%.1f")°F")
-                    .font(.largeTitle)
-                    .bold()
-
-                Text(weather.current.condition.text)
-                    .font(.headline)
-                    .padding()
-
-                AsyncImage(url: URL(string: "https:\(weather.current.condition.icon)")) { image in
-                    image.resizable().frame(width: 100, height: 100)
-                } placeholder: {
-                    ProgressView()
+                    //.font(.title)
+                    .padding(.top,20)
+                Text("Today, \(formattedDate())")
+                    .font(.system(size: 13))
+                     .foregroundColor(.gray)
+                     .padding(.top, 15)
+                HStack(alignment: .top, spacing: 2) {
+                    Text("\(weather.current.temp_c, specifier: "%.1f")")
+                        .font(.system(size: 70, weight: .bold))
+                    Text("°C")
+                        .font(.system(size: 24)) // Smaller °C
+                        .padding(.top, 6) // Push °C slightly to the top
                 }
-                Button("Test") {
+                .padding(.top, 20)
+                HStack{
+                    AsyncImage(url: URL(string: "https:\(weather.current.condition.icon)")) { image in
+                        image.resizable().frame(width: 100, height: 100)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Text(weather.current.condition.text)
+                        .font(.headline)
+                        //.padding()
+                  
+                }
+              
+                Spacer()
+               
+
+               
+
+              
+                Button("Upcoming Days") {
                     Task {
                         await viewModel.loadForecastWeather(for: "Bangalore", days:14)
                         print(viewModel.forecastWeather?.forecast.forecastday ?? "empty")
@@ -54,7 +70,7 @@ struct WeatherView: View {
                     }
             }
         }
-        .padding()
+        .padding(.horizontal)
     }
 }
 
@@ -62,4 +78,10 @@ struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherView()
     }
+}
+
+private func formattedDate() -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "MMM dd h:mm a" // Example: Feb 23, 5:30 PM
+    return formatter.string(from: Date())
 }
